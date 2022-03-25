84ebe346c5c49bb52d856e813adee6ea7823f9e6
willtop <billcuitop@gmail.com>
2022-03-25 19:50:39 -0400

    revert back to beamforming gains (as in robust optimization project)

--- D2D/setup.py
+++ D2D/setup.py
@@ -33,7 +33,14 @@ TX_POWER = np.power(10, (_TX_POWER_dBm - 30) / 10)
 SETTING_STRING = "N{}_L{}_{}-{}m".format(N_LINKS, FIELD_LENGTH, SHORTEST_DIRECTLINK, LONGEST_DIRECTLINK)
 SINR_GAP_dB = 0
 SINR_GAP = np.power(10, SINR_GAP_dB/10)
-ANTENNA_GAIN_DB = 6
+# Transmitter and receiver setting
+N_ANTENNAS = 8
+ANTENNA_SEPARATION = WAVELENGTH/2
+# Transmitter and receiver beamforming gains (consistent with the above settings)
+DIRECTLINK_GAIN_dB = 9 # the gain for direct transmitters and receivers
+MAIN_LOBE_GAIN_dB = 6
+SIDE_LOBE_GAIN_dB = -9
+MAIN_LOBE_HALF_WIDTH = np.pi*(10/180)
 
 # number of samples 
 # Note: the testing layouts generated in the "MinRate" is used to test both sum rate and min rate
--- D2D/utils.py
+++ D2D/utils.py
@@ -2,6 +2,16 @@ import numpy as np
 from setup import *
 from tqdm import trange
 
+# Compute the angle based on three points
+def compute_angle(center_loc, loc1, loc2):
+    vec1, vec2 = loc1-center_loc, loc2-center_loc
+    assert np.linalg.norm(vec1)>0 and np.linalg.norm(vec2)>0
+    vec1, vec2 = vec1/np.linalg.norm(vec1), vec2/np.linalg.norm(vec2)
+    dotprod = np.dot(vec1, vec2)
+    assert dotprod > -1-1e-3 and dotprod < 1+1e-3
+    angle = np.arccos(np.clip(dotprod, -1.0, 1.0))
+    return angle
+
 # Generate layout one at a time
 def generate_one_D2D_layout():
     # first, generate transmitters' coordinates
@@ -20,9 +30,10 @@ def generate_one_D2D_layout():
                 if(0<=rx_x<=FIELD_LENGTH and 0<=rx_y<=FIELD_LENGTH):
                     got_valid_rx = True
             rx_xs.append(rx_x); rx_ys.append(rx_y)
-        # For now, assuming equal weights and equal power, so not generating them
         layout = np.concatenate((tx_xs, tx_ys, rx_xs, rx_ys), axis=1)
         distances = np.zeros([N_LINKS, N_LINKS])
+        angles_rx = np.zeros([N_LINKS, N_LINKS])
+        angles_tx = np.zeros([N_LINKS, N_LINKS])
         # compute distance between every possible Tx/Rx pair
         for rx_index in range(N_LINKS):
             for tx_index in range(N_LINKS):
@@ -30,23 +41,29 @@ def generate_one_D2D_layout():
                 rx_coor = layout[rx_index][2:4]
                 # according to paper notation convention, Hij is from jth transmitter to ith receiver
                 distances[rx_index][tx_index] = np.linalg.norm(tx_coor - rx_coor)
+                # compute the angle from rx perspective
+                angles_rx[rx_index][tx_index] = compute_angle(center_loc=layout[rx_index][2:4], loc1=layout[rx_index][0:2], loc2=layout[tx_index][0:2])
+                angles_tx[rx_index][tx_index] = compute_angle(center_loc=layout[tx_index][0:2], loc1=layout[tx_index][2:4], loc2=layout[rx_index][2:4])
         if(np.min(distances+np.eye(N_LINKS)*SHORTEST_CROSSLINK)<SHORTEST_CROSSLINK):
             pass
         else:
             break # go ahead and return the layout
-    return layout, distances
+    return layout, distances, angles_rx, angles_tx
 
 def generate_D2D_layouts(n_layouts):
     print("<<<<<<<<<<<<<{} layouts: {}>>>>>>>>>>>>".format(n_layouts, SETTING_STRING))
-    layouts_all, distances_all = [], []
+    layouts_all, distances_all, angles_rx_all, angles_tx_all = [], [], [], []
     for i in trange(n_layouts):
-        layouts, distances = generate_one_D2D_layout()
+        layouts, distances, angles_rx, angles_tx = generate_one_D2D_layout()
         layouts_all.append(layouts)
         distances_all.append(distances)
-    layouts_all, distances_all = np.array(layouts_all), np.array(distances_all)
+        angles_rx_all.append(angles_rx)
+        angles_tx_all.append(angles_tx)
+    layouts_all, distances_all, angles_rx_all, angles_tx_all = \
+            np.array(layouts_all), np.array(distances_all), np.array(angles_rx_all), np.array(angles_tx_all)
     assert np.shape(layouts_all)==(n_layouts, N_LINKS, 4)
-    assert np.shape(distances_all)==(n_layouts, N_LINKS, N_LINKS)
-    return layouts_all, distances_all
+    assert np.shape(distances_all)==np.shape(angles_rx_all)==np.shape(angles_tx_all)==(n_layouts, N_LINKS, N_LINKS)
+    return layouts_all, distances_all, angles_rx_all, angles_tx_all
 
 def generate_D2D_channelGains(n_layouts):
     layouts, distances = generate_D2D_layouts(n_layouts)
@@ -60,7 +77,15 @@ def generate_D2D_channelGains(n_layouts):
     # compute coefficient matrix for each Tx/Rx pair
     sum_term = 20 * np.log10(distances / Rbp)
     Tx_over_Rx = Lbp + 6 + sum_term + ((distances > Rbp).astype(int)) * sum_term  # adjust for longer path loss
-    pathLosses = -Tx_over_Rx + np.eye(N_LINKS) * ANTENNA_GAIN_DB  
+    # add beamforming gain based on directions
+    Tx_gains = (np.ones([N_LINKS, N_LINKS])*MAIN_LOBE_HALF_WIDTH >= angles_tx) * MAIN_LOBE_GAIN_dB + \
+               (np.ones([N_LINKS, N_LINKS])*MAIN_LOBE_HALF_WIDTH <= angles_tx) * SIDE_LOBE_GAIN_dB
+    Rx_gains = (np.ones([N_LINKS, N_LINKS])*MAIN_LOBE_HALF_WIDTH >= angles_rx) * MAIN_LOBE_GAIN_dB + \
+               (np.ones([N_LINKS, N_LINKS])*MAIN_LOBE_HALF_WIDTH <= angles_rx) * SIDE_LOBE_GAIN_dB
+    # add beamforming gains
+    pathLosses = -Tx_over_Rx + Tx_gains + Rx_gains 
+    # add the best beamforming gains only for direct channels
+    pathLosses = pathLosses + np.eye(N_LINKS) * (DIRECTLINK_GAIN_dB-MAIN_LOBE_GAIN_dB)
     pathLosses = np.power(10, (pathLosses / 10))  # convert from decibel to absolute
     ############# Shadowing and Fast Fading ##########
     # generate shadowing coefficients
