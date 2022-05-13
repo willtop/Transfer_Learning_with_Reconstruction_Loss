2bf261f38f4eebc6f2615c067168b7b77123c543
Wei Cui <billcuitop@gmail.com>
2022-05-13 10:11:30 -0400

    setting still not showing much improvement for harmonic->sum (1% improve)

--- D2D/neural_nets.py
+++ D2D/neural_nets.py
@@ -9,7 +9,7 @@ class Neural_Net(nn.Module):
     def __init__(self):
         super().__init__()
         # model architecture attribute
-        self.feature_length = int(N_LINKS * N_LINKS * 0.5)
+        self.feature_length = int(N_LINKS * N_LINKS * 0.75)
         # attributes to be overridden by subclasses
         self.model_type = None
         self.model_path = None
