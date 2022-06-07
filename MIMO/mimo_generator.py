673e0f238dffabbb4bb934b7917983506e2ffdbc
Wei Cui <billcuitop@gmail.com>
2022-06-07 15:34:11 -0400

    keep coding mimo code base

--- MIMO/mimo_generator.py
+++ MIMO/mimo_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 from setup import *
 
 def get_channels_to_BS(ue_locs, BS_ID):
@@ -6,18 +7,34 @@ def get_channels_to_BS(ue_locs, BS_ID):
     # get coordinate differences between UE of all networks and three fixed-location BSs
     ue_bs_coord_diffs = ue_locs - BS_LOCATIONS[BS_ID] 
     # let theta denote the azimuth angle, and phi denote the elevation angle
-    # assume linear array of BS antennas lining in the direction of (0,1,0)
-    # assume all BS antennas point towards (0,0,1) direction with negligible length
-    # the single travel difference for the steering angle would be:
+    # assume 2D BS antenna array lining in x-y plane with negligible antenna length
+    # the singal travel difference for the steering angle would be:
+    # along the y-axis:
     # antenna_spacing X cos(theta) X cos(phi) = antenna_spacing X (y_UE-y_BS)/distance_UE->BS
+    # along the x-axis:
+    # antenna_spacing X sin(theta) X cos(phi) = antenna_spacing X (x_UE-x_BS)/distance_UE->BS
     ue_bs_dists = np.linalg.norm(ue_bs_coord_diffs, axis=1)
-    steer_vec_tmp = ue_bs_coord_diffs[:,1]/ue_bs_dists
-    # incorporate antenna indexing: antenna to the right-most (highest y coordinate) is indexed the first
-    steer_vec_tmp = np.arange(N_BS_ANTENNAS)*np.expand_dims(steer_vec_tmp,axis=1)
+    steer_vec_tmp_x = ue_bs_coord_diffs[:,0]/ue_bs_dists
+    steer_vec_tmp_y = ue_bs_coord_diffs[:,1]/ue_bs_dists
+    # incorporate antenna indexing: 
+    # flatten all antenna indexing into one vector with row-major:
+    # each row index antennas with same x-coordinate, with first row indexing highest x-coordinate
+    # in each row, antenna to the right most (highest y coordinate) is indexed the first
+    steer_vec_tmp = np.concatenate([np.expand_dims(steer_vec_tmp_x,axis=1),
+                                    np.expand_dims(steer_vec_tmp_y, axis=1)],axis=1)
+    indices = np.transpose(np.array(list(itertools.product(np.arange(np.sqrt(N_BS_ANTENNAS)), repeat=2))))
+    steer_vec_tmp = np.matmul(steer_vec_tmp, indices)
+    assert np.shape(steer_vec_tmp) == (n_networks, N_BS_ANTENNAS)
     channels_LOS = np.exp(ANTENNA_SPACING_PHASE_SHIFT*steer_vec_tmp)  
-    assert np.shape(channels_LOS) == (n_networks, N_BS_ANTENNAS)
     channels_NLOS = np.random.normal(size=(n_networks, N_BS_ANTENNAS)) + 1j * np.random.normal(size=(n_networks, N_BS_ANTENNAS))
-    
+    channels_fading = np.sqrt(RICIAN_FACTOR/(1+RICIAN_FACTOR))*channels_LOS + \
+                        np.sqrt(1/(1+RICIAN_FACTOR))*channels_NLOS
+    # compute path losses
+    ue_bs_dists_tmp = np.tile(np.expand_dims(ue_bs_dists, axis=1), (1, N_BS_ANTENNAS))
+    pathlosses = 32.6 + 36.7*np.log(ue_bs_dists_tmp)
+    assert np.shape(pathlosses)==np.shape(channels_fading)==(n_networks, N_BS_ANTENNAS)
+    channels_one_BS = pathlosses*channels_fading
+    return channels_one_BS
 
 def generate_MIMO_networks(n_networks):
     # generate user location in 3D space
@@ -26,10 +43,14 @@ def generate_MIMO_networks(n_networks):
                              np.random.uniform(low=0, high=FIELD_HEIGHT, size=(n_networks, 1))], axis=1) 
     assert np.shape(ue_locs) == (n_networks, 3)
     # compute channels from the UEs to each of the BSs
+    channels = []
     for i in range(N_BS):
-        get_channels_to_BS(ue_locs, i)
-    
-    return ue_locs
+        channels_one_BS = get_channels_to_BS(ue_locs, i)
+        assert np.shape(channels_one_BS)==(n_networks, N_BS_ANTENNAS)
+        channels.append(np.expand_dims(channels_one_BS,axis=1))
+    channels = np.concatenate(channels, axis=1)
+    assert np.shape(n_networks, N_BS, N_BS_ANTENNAS)
+    return ue_locs, channels
 
 if __name__=="__main__":
-
+    
--- MIMO/setup.py
+++ MIMO/setup.py
@@ -4,7 +4,9 @@ import torch
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device for torch: ", DEVICE)
 
-
+"""
+Environment Setting
+"""
 N_BS = 3
 N_BS_ANTENNAS = 16
 ANTENNA_SPACING_PHASE_SHIFT = 1
@@ -16,3 +18,11 @@ assert np.shape(BS_LOCATIONS) == (N_BS, 3)
 BANDWIDTH = 5e6
 _NOISE_dBm_Hz = -150
 NOISE_POWER = np.power(10, ((_NOISE_dBm_Hz-30)/10)) * BANDWIDTH
+RICIAN_FACTOR = 10
+
+# ensure the number of antennas is a perfect square, for 2D array
+assert np.floor(np.sqrt(N_BS_ANTENNAS))**2 == N_BS_ANTENNAS
+
+"""
+Training Setting
+"""
